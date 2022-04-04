cb0916536c2b7bb7b660344a0b00cc63e8849807
NiGaN <yusniel.abreu@gmail.com>
2022-04-04 18:20:36 -0400

    Add files via upload

--- config.py
+++ config.py
@@ -3,16 +3,18 @@ API_SECRET_KEY = ""
 API_PASSPHRASE = ""
 
 
-SYMBOL = "ETH-USDT"
+SYMBOL = "BTC-USDT"
 
 INVERSION_TOTAL_USDT=300 #INVERSION TOTAL EN USDT PARA LOS BOTS
 
- 
 # gridbot settings
-NUM_BUY_GRID_LINES = 5 #ordenes de compra
-NUM_SELL_GRID_LINES = 5 #ordenes de venta
-GRID_SIZE = 100 #diferencia de precios entre ordenes
+MAX_RANGE=50000
+MIN_RANGE=40000
+
+
+GRID_AMOUNT=50 #grid para ponerle al bot
+GRID_SIZE = int(MAX_RANGE-MIN_RANGE)//GRID_AMOUNT     #diferencia de precios entre ordenes
 
 CHECK_ORDERS_FREQUENCY = 0.5 #frecuencia para revisar las ordenes
 
-INVEST_SIZE=INVERSION_TOTAL_USDT//(NUM_BUY_GRID_LINES+NUM_SELL_GRID_LINES) #cantidad en usd para comprar y vender en el sgrid
+INVEST_SIZE=INVERSION_TOTAL_USDT//GRID_AMOUNT #cantidad en usd para comprar y vender en el sgrid
